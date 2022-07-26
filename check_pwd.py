854dacac48f49ed36e0332fd141efc4716af6769
Ollaradam <ollara@oregonstate.edu>
2022-07-25 19:20:32 -0700

    Corrected to return false for no uppercase character present

--- check_pwd.py
+++ check_pwd.py
@@ -2,4 +2,6 @@ def check_pwd(pwd):
     symbols = '~`!@#$%^&*()_+-='
     if len(pwd) < 8 or len(pwd) > 20:
         return False
+    if not any(char.isupper() for char in pwd):
+        return False
     return True
