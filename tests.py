f8ea48891e1110ae846c0560b1e3399c5a515c78
Ollaradam <ollara@oregonstate.edu>
2022-07-25 19:07:42 -0700

    Test for if the password is too long

--- tests.py
+++ tests.py
@@ -4,8 +4,11 @@ from check_pwd import check_pwd
 
 class TestCase(unittest.TestCase):
 
-  def test1(self):
-    self.assertFalse(check_pwd('apply'))
+    def test1(self):
+        self.assertFalse(check_pwd('apply'))
+
+    def test2(self):
+        self.assertFalse(check_pwd('antidisestablishmentarianism'))
 
 
 if __name__ == '__main__':
